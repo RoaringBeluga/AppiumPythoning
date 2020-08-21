d81a903046b18e6e4e22b66c740096c93aac2e28
RoaringBeluga <admin@stentorian-belu.ga>
2020-08-21 12:38:05 +0800

    Typos!!!

--- android_time_selection_test.py
+++ android_time_selection_test.py
@@ -8,12 +8,12 @@ import os
 
 @pytest.fixture()
 def driver():
-    appdir = os.getcwd() + "/Apps/TestApp.app.zip"
+    appdir = os.getcwd() + "/Apps/ApiDemos-debug.apk"
     print(appdir)
     caps = {}
-    caps["deviceName"] = "iPhone SE (2nd generation)"
-    caps["platformName"] = "iOS"
-    caps["automationName"] = "XCUITest"
+    caps["deviceName"] = "Pixel_3a_API_29"
+    caps["platformName"] = "Android"
+    caps["automationName"] = "UiAutomator2"
     caps["app"] = appdir
 
     driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
@@ -25,25 +25,25 @@ def driver():
 
 
 def test_date_selector(driver):
-    driver.find_element_by_accesibility_id("Views").click()
-    driver.find_element_by_accesibility_id("Date Widgets").click()
-    driver.find_element_by_accesibility_id("1. Dialog").click()
-    driver.find_element_by_accesibility_id("change the time").clkick()
+    driver.find_element_by_accessibility_id("Views").click()
+    driver.find_element_by_accessibility_id("Date Widgets").click()
+    driver.find_element_by_accessibility_id("1. Dialog").click()
+    driver.find_element_by_accessibility_id("change the time").clkick()
 
-    initial_hours = driver.find_element_by_accesibility_id("android:id/hours").text
-    initial_minutes = driver.find_element_by_accesibility_id("android:id/minutes").text
+    initial_hours = driver.find_element_by_accessibility_id("android:id/hours").text
+    initial_minutes = driver.find_element_by_accessibility_id("android:id/minutes").text
 
     assert initial_hours == "1"
     assert initial_minutes == "02"
 
-    driver.find_element_by_accesibility_id("7").click()
-    driver.find_element_by_accesibility_id("40").click()
+    driver.find_element_by_accessibility_id("7").click()
+    driver.find_element_by_accessibility_id("40").click()
 
-    new_hours = driver.find_element_by_accesibility_id("android:id/hours").text
-    new_minutes = driver.find_element_by_accesibility_id("android:id/minutes").text
+    new_hours = driver.find_element_by_accessibility_id("android:id/hours").text
+    new_minutes = driver.find_element_by_accessibility_id("android:id/minutes").text
 
     assert new_hours == "7"
     assert new_minutes == "40"
 
-    time_header = driver.find_element_by_accesibility_id("android:id/time_header").text
+    time_header = driver.find_element_by_accessibility_id("android:id/time_header").text
     print(time_header)
