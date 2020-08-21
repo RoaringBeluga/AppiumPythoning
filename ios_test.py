697ebb92d06ac8bb9aea730f23194fc35e9d2e19
RoaringBeluga <admin@stentorian-belu.ga>
2020-08-21 10:16:39 +0800

    Edited for pytest compatibility

--- ios_test.py
+++ ios_test.py
@@ -5,29 +5,60 @@
 from appium import webdriver
 import pytest
 
-caps = {}
-caps["deviceName"] = "iPhone SE (2nd generation)"
-caps["platformName"] = "iOS"
-caps["automationName"] = "XCUITest"
-caps["app"] = "/Users/desman/Development/Python/Appium/Apps/TestApp.app.zip"
+def test_positive_numbers():
+    caps = {}
+    caps["deviceName"] = "iPhone SE (2nd generation)"
+    caps["platformName"] = "iOS"
+    caps["automationName"] = "XCUITest"
+    caps["app"] = "/Users/desman/Development/Python/Appium/Apps/TestApp.app.zip"
 
-driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
-print("Driver!")
-el1 = driver.find_element_by_accessibility_id("IntegerA")
-el1.click()
-print("Integer A tapped!")
-el1.send_keys("4")
-print("Integer A entered!")
-el2 = driver.find_element_by_accessibility_id("IntegerB")
-el2.click()
-print("Integer B tapped!")
-el2.send_keys("5")
-print("Integer B entered!")
-el3 = driver.find_element_by_accessibility_id("ComputeSumButton")
-el3.click()
-print("Compute Sum tapped!")
-el4 = driver.find_element_by_accessibility_id("Answer")
-print(el4.text)
-print("'Answer' element value printed!")
+    driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+    print("Driver!")
+    integer_a = driver.find_element_by_accessibility_id("IntegerA")
+    integer_a.click()
+    print("Integer A tapped!")
+    integer_a.send_keys("4")
+    print("Integer A entered!")
+    integer_b = driver.find_element_by_accessibility_id("IntegerB")
+    integer_b.click()
+    print("Integer B tapped!")
+    integer_b.send_keys("5")
+    print("Integer B entered!")
+    compute_sum = driver.find_element_by_accessibility_id("ComputeSumButton")
+    compute_sum.click()
+    print("Compute Sum tapped!")
+    answer = driver.find_element_by_accessibility_id("Answer")
+    print(answer.text)
+    print("'Answer' element value printed!")
 
-driver.quit()
+    assert int(answer.text) > 0
+    driver.quit()
+
+def test_negative_numbers():
+    caps = {}
+    caps["deviceName"] = "iPhone SE (2nd generation)"
+    caps["platformName"] = "iOS"
+    caps["automationName"] = "XCUITest"
+    caps["app"] = "./Apps/TestApp.app.zip"
+
+    driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+    print("Driver!")
+    integer_a = driver.find_element_by_accessibility_id("IntegerA")
+    integer_a.click()
+    print("Integer A tapped!")
+    integer_a.send_keys("-1")
+    print("Integer A entered!")
+    integer_b = driver.find_element_by_accessibility_id("IntegerB")
+    integer_b.click()
+    print("Integer B tapped!")
+    integer_b.send_keys("-3")
+    print("Integer B entered!")
+    compute_sum = driver.find_element_by_accessibility_id("ComputeSumButton")
+    compute_sum.click()
+    print("Compute Sum tapped!")
+    answer = driver.find_element_by_accessibility_id("Answer")
+    print(answer.text)
+    assert int(answer.text) < 0
+    print("'Answer' element value printed!")
+
+    driver.quit()
