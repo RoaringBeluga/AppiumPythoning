549fc9c76c137498e68842e2478cc449775a3bbf
RoaringBeluga <alexander@nowherelands.net>
2020-09-02 09:54:29 +0800

    Removed unused capability

--- conftest.py
+++ conftest.py
@@ -4,11 +4,19 @@ from appium import webdriver
 import pytest
 import os
 
+
 failed_test_count = 0
 
-@pytest.fixture()
+
 def failed_tests():
+    global failed_test_count
     return failed_test_count
+
+
+def increase_failures():
+    global failed_test_count
+    failed_test_count+=1
+
 
 appsdir = os.getcwd() + "/Apps/"
 screenshot_dir = os.getcwd() + "/Screenshots/"
@@ -26,13 +34,16 @@ def pytest_addoption(parser):
     parser.addoption("--devicename", action="store", default="iPhone SE (2nd generation)", help="Device name")
     parser.addoption("--app", action="store", default=ios_test_app_name)
 
+
 @pytest.fixture()
 def app(request):
     return request.config.getoption("--app")
 
+
 @pytest.fixture()
 def platform(request):
     return request.config.getoption("--platform").lower()
+
 
 @pytest.fixture()
 def device_name(request):
@@ -40,12 +51,13 @@ def device_name(request):
 
 
 @pytest.fixture()
-def driver(platform, device_name, app, failed_tests, request):
+def driver(platform, device_name, app, request):
+    global failed_test_count
     """
     Returns WebDriver instance with required capabilities.
     """
     caps = {}
-    caps["deviceName"] = device_name # Device name of the emulator
+    caps["deviceName"] = device_name  # Device name of the emulator
     caps["platformName"] = platform
     caps["newCommandTimeout"] = 360
     if platform == 'ios':
@@ -57,20 +69,19 @@ def driver(platform, device_name, app, failed_tests, request):
         # "udid": "JBAXB765F0793AA",  # udid of the physical device
         # caps["platformName"] = "Android"
         caps["automationName"] = "UiAutomator2"
-        caps["androidDeviceReadyTimeout"] = 50000  # Waiting for the device to become available – AVD startup times are slooow
         caps["app"] = appsdir + app  # Find test app in the Apps directory next to our tests
         caps["avd"] = "Pixel_3a_API_29"  # run the AVD with this name.
     else:
         pytest.fail(msg="Platform not supported!")
-    print("Platform: " , platform, "\tDevice: ", device_name)
+    print("Platform: ", platform, "\tDevice: ", device_name)
     print("Capabilities: ", caps)
     driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
     print("Driver initialized")
     yield driver
     # pytest.set_trace()
-    if request.session.testsfailed > failed_tests:
+    if request.session.testsfailed > failed_test_count:
         file_name = screenshot_dir + platform + "/" + timestamp() + "_" + request.node.name + ".png"
         driver.save_screenshot(file_name)
-        failed_tests+=1
+        failed_test_count+=1
     driver.quit()
     print("Driver killed.")
---
+++ testing_test.py
@@ -0,0 +1,14 @@
+def test_once(driver):
+    assert 0
+
+
+def test_twice(driver):
+    assert True
+
+
+def test_thrice(driver):
+    assert False
+
+
+def test_more(driver):
+    assert True
